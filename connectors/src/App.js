e9465b1fff6547d3ada0fc83d103ede47aed8e63
Preeti Rajguru <98033809+PreetiRajguru@users.noreply.github.com>
2023-08-11 17:00:03 +0530

    refactor: replaced svg with icons

--- connectors/src/App.js
+++ connectors/src/App.js
@@ -5,31 +5,18 @@ import HomePage from './Components/HomePage';
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import "./style.css";
-import Configure from './Components/Configure';
+import "bootstrap-icons/font/bootstrap-icons.min.css";
 
+import "./style.css";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/configure" element={<Configure />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
-// import React from 'react';
-
-// const HomePage = () => {
-//   return (
-//     // <div>
-//     //   <h1>Hi there!</h1>
-//     // </div>
-//   );
-// };
-
-// export default HomePage;
